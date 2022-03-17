1c6f4ad490bc5efcb563c8fb3769282adacd6fa4
Peter Goldstein <peter.m.goldstein@gmail.com>
2022-03-17 16:18:48 -0700

    Add migration path

--- spec/dummy/lib/dummy.rb
+++ spec/dummy/lib/dummy.rb
@@ -7,3 +7,12 @@ module Dummy
   require_relative "../config/environment"
   require_relative "../app/models/user.rb"
 end
+
+migrate_path = File.expand_path("../db/migrate/", __FILE__)
+if ActiveRecord::VERSION >= '6.0'
+  ActiveRecord::MigrationContext.new(migrate_path, ActiveRecord::SchemaMigration).migrate
+elsif ActiveRecord::VERSION >= '5.2'
+  ActiveRecord::MigrationContext.new(migrate_path).migrate
+else
+  ActiveRecord::Migrator.migrate(migrate_path)
+end
